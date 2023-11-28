f532b74d3bb5520c6a3c6a5a07793b1097e93b43
piyush-ramnani <test@Git.com>
2023-11-28 13:09:54 +0530

    Strings | Map() | arrays

--- function-problems/uppercaseArray.js
+++ function-problems/uppercaseArray.js
@@ -13,3 +13,10 @@ function toUppercase(A) {
 const A = ["piyush", "samrat", "sahil"];
 
 toUppercase(A);
+
+/*
+    forEach((el)=>{el = el.toUpperCase()}) wouldn't work here. 
+    Each element is converted to uppercase within the callback function, but these changes are not reflected outside of the callback. Therefore we need to map the elements to a new array.
+
+    NOTE: A normal for loop used to iterate over an array and modify its elements will not inherently modify the original array if you're working with primitive values like STRINGS or NUMBERS. In JavaScript, primitive values are passed by value, so modifying the loop variable inside the loop doesn't affect the original array elements.
+*/
